e76648f48e420a619723964f0204c1439a80b044
Rizqi Djamaluddin <rizqidjamaluddin@gmail.com>
2018-06-25 01:25:11 +0700

    Add search clear button

--- src/views/browser/support/BrowserSearch.vue
+++ src/views/browser/support/BrowserSearch.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="search text-sm">
+  <div class="search text-sm relative">
     <span class="uppercase text-black font-bold mr-2">Search</span>
     <input type="text" @input="update($event.target.value)"
     class="text-blue p-2 border border-ash rounded"
     :value="search" placeholder="Find a job">
+    <div class="clear absolute p-2 cursor-pointer" @click="clear()"
+         v-bind:class="{'text-ash': !search, 'text-black': search}">
+      &#x2716;
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -21,11 +25,22 @@ export default class BrowserSearch extends Vue {
     update(term: string) {
       this.$emit('update:search', term);
     }
+
+    clear() {
+      this.$emit('update:search', '');
+    }
 }
 </script>
 <style lang="scss">
   .search {
     position: relative;
     top: -1.5px;
+  }
+
+  .clear {
+    position: absolute;
+    right: 5px;
+    // 1 extra pixel bump to compensate for the search box border
+    top: 1px;
   }
 </style>
